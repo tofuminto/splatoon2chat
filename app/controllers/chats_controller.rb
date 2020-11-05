1aed5f4110b598f4fab7cbd93cdf8b8fbe048037
tofuminto <tototofumifumin@gmail.com>
2020-11-05 22:00:33 +0900

    main_chat_viewsfile

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,24 @@
 class ChatsController < ApplicationController
 
   def index
+    @message = Chat.find(1)
   end
 
-  # private
+  def new
+    @message = Chat.new
+  end
 
-    # def chat_params
-    #   params.require(:chat).permit(:title, :content)
-    # end
+  def create
+    @message = Chat.new(chat_params)
+    if @message.save!(chat_params)
+      redirect_to root_path
+    else
+      render "index"
+    end
+  end
 
+  private
+  def chat_params
+    params.require(:chat).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root "chats#index"
-  resources :chats, only: [:index]
+  resources :chats, only: [:index, :create]
 end
