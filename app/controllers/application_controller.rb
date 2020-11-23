8467c48c7cb7b80c11395a8dde6b00e4d4aa9da1
tofuminto <tototofumifumin@gmail.com>
2020-11-24 07:04:00 +0900

    edit_users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_host
 
+  def set_host
+    Rails.application.routes.default_url_options[:host] = request.host_with_port
+  end
+  
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
+    added_attrs = [:nickname, :email, :password, :password_confimation, :remember_me]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
   end
 end
