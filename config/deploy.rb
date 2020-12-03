f5a5b094eab8da29ba5ad8fa5c727e1bc3e10971
tofuminto <tototofumifumin@gmail.com>
2020-12-04 01:14:11 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, "splatoon2chat"
 set :repo_url, "git@github.com:tofuminto/splatoon2chat.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', '.bundler', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', '.bundle', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
